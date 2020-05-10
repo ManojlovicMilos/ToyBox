5232ee3aa7934ec449eecbffba84d3cb0057689d
Manojlovic Milos <manojlovicsmilos@hotmail.com>
2020-05-10 16:42:19 +0200

    Build Changes [0.2.2]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.2.1";
+    public static Version:string = "0.2.2";
     public static LibPath:string = "Resources/";
     public static Graphics:Quality = Quality.High;
     public static EngineUIStyle:boolean = true;
