cfc40cbdf2f61929dac5f20cbbbbd3e6b7e0505e
manojlovicsmilos <manojlovicsmilos@hotmail.com>
2026-01-13 15:25:11 +0100

    Build Changes [0.2.4]

--- app/Engine/Settings.ts
+++ app/Engine/Settings.ts
@@ -8,7 +8,7 @@ enum Quality
 }
 class Settings
 {
-    public static Version:string = "0.2.4-beta.1";
+    public static Version:string = "0.2.4";
     public static LibPath:string = "Resources/";
     public static Graphics:Quality = Quality.High;
     public static EngineUIStyle:boolean = true;
